fe2137260b990b595cb502f2646ca19d7d955b13
M.Mimpen <m.mimpen@hotmail.com>
2011-09-19 17:56:57 +0200

    Testen met EF

---
+++ DigitaalKruis/Controllers/ContactController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DigitaalKruis;
+using DigitaalKruis.Models;
+
+namespace DigitaalKruis.Controllers
+{ 
+    public class ContactController : Controller
+    {
+        private KaartenbakEntities db = new KaartenbakEntities();
+
+        //
+        // GET: /Contact/
+
+        public ViewResult Index()
+        {
+            return View(db.Contacts.ToList());
+        }
+
+        //
+        // GET: /Contact/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Contact contact = db.Contacts.Single(c => c.ContactID == id);
+            return View(contact);
+        }
+
+        //
+        // GET: /Contact/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Contact/Create
+
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.AddObject(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(contact);
+        }
+        
+        //
+        // GET: /Contact/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Contact contact = db.Contacts.SingleOrDefault(c => c.ContactID == id);
+            return View(contact);
+        }
+
+        //
+        // POST: /Contact/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Attach(contact);
+                db.ObjectStateManager.ChangeObjectState(contact, EntityState.Modified);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        //
+        // GET: /Contact/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Contact contact = db.Contacts.Single(c => c.ContactID == id);
+            return View(contact);
+        }
+
+        //
+        // POST: /Contact/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Contact contact = db.Contacts.Single(c => c.ContactID == id);
+            db.Contacts.DeleteObject(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DigitaalKruis/Models/ContactModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DigitaalKruis.Models
+{
+    [MetadataType(typeof(Contact_Validation))]
+    public partial class Contact
+    { }
+
+    public class Contact_Validation
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public Nullable<global::System.DateTime> Birthdate { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public global::System.String Phone { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string PasswordSalt { get; set; }
+    }
+}
